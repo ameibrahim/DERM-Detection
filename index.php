de640e9c68231964280657be96369736f2c49db9
Ibrahim Ame <ame.ibrahim@yahoo.com>
2024-01-04 10:45:56 +0200

    MaxWidth 800

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DERM Detect</title>
-    <link rel="stylesheet" href="css/main.css?20">
+    <link rel="stylesheet" href="css/main.css?21">
     <link rel="stylesheet" href="css/popup.css?6">
     <link rel="stylesheet" href="css/input.css?2">
 
